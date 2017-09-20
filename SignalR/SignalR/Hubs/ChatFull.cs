f6970aeb39ada563f70be0625ca4309f80330532
lucaspolaquini <cmte.lucas@hotmail.com>
2017-09-20 20:59:28 -0300

    Inicial

---
+++ SignalR/SignalR/Hubs/ChatFull.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatFull :Hub
+    {
+        public Task EnviarMensagem(Mensagem Mensagem)
+        {
+            return Clients.All.InvokeAsync("publicarMensagem", Mensagem);
+        }
+    }
+
+    public class Mensagem
+    {
+        public String Nome { get; set; }
+
+        public String Texto { get; set; }
+    }
+}
---
+++ SignalR/SignalR/Hubs/chat.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+    public class Chat : Hub
+    {
+        public Task Send(String Msg)
+        {
+            return Clients.All.InvokeAsync("Send", Msg);
+        }
+    }
+}
